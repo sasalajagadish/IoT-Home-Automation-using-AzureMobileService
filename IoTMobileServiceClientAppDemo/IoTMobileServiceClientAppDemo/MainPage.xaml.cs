2dfdf723939fda84ecda368f283c7c837aeac41f
romilbheda <romilbheda@hotmail.com>
2016-02-11 14:07:54 +0530

    Home Automation using Mobile Service - Client project

---
+++ IoTMobileServiceClientAppDemo/IoTMobileServiceClientAppDemo/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IoTMobileServiceClientAppDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        IMobileServiceTable<powerstatus> userTable = App.MobileService.GetTable<powerstatus>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btnOn_Click(object sender, RoutedEventArgs e)
+        {
+            List<powerstatus> datalist = await App.MobileService.GetTable<powerstatus>().ToListAsync();
+            powerstatus obj = new powerstatus();
+            foreach (var x in datalist)
+            {
+                obj.id = x.id;
+                obj.status = "on";
+            }
+            await userTable.UpdateAsync(obj);
+
+        }
+
+        private async void btnOff_Click(object sender, RoutedEventArgs e)
+        {
+            List<powerstatus> datalist = await App.MobileService.GetTable<powerstatus>().ToListAsync();
+            powerstatus obj = new powerstatus();
+            foreach (var x in datalist)
+            {
+                obj.id = x.id;
+                obj.status = "off";
+            }
+            await userTable.UpdateAsync(obj);
+        }
+
+        private async void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            powerstatus obj1 = new powerstatus();
+            obj1.status = "on";
+
+            await userTable.InsertAsync(obj1);
+        }
+    }
+}
---
+++ IoTMobileServiceClientAppDemo/IoTMobileServiceClientAppDemo/powerstatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTMobileServiceClientAppDemo
+{
+    public class powerstatus
+    {
+        public string id { get; set; }
+
+        public string status { get; set; }
+    }
+}
