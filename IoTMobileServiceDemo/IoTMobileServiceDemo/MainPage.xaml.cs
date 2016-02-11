dc7b1c5f4509b4f2e259c388fc6dbdfc0b9c42de
sasalajagadish <jagadish@getwings.co>
2016-02-11 14:11:59 +0530

    Home Automation using Mobile Service

---
+++ IoTMobileServiceDemo/IoTMobileServiceDemo/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IoTMobileServiceDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private const int LED_PIN = 5;
+        private const int LED_PIN1 = 4;
+        private GpioPin pin;
+        private GpioPinValue pinValue;
+        private DispatcherTimer timer;
+        private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+        private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Tick += Timer_Tick;
+            InitGPIO();
+            if (pin != null)
+            {
+                timer.Start();
+            }
+        }
+
+        private void InitGPIO()
+        {
+            var gpio = GpioController.GetDefault();
+
+            // Show an error if there is no GPIO controller
+            if (gpio == null)
+            {
+                pin = null;
+                GpioStatus.Text = "There is no GPIO controller on this device.";
+                return;
+            }
+
+            pin = gpio.OpenPin(LED_PIN);
+            pinValue = GpioPinValue.High;
+            pin.Write(pinValue);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+
+            GpioStatus.Text = "GPIO pin initialized correctly.";
+        }
+
+        public string value;
+
+        private async void Timer_Tick(object sender, object e)
+        {
+            List<powerstatus> datalist = await App.MobileService.GetTable<powerstatus>().ToListAsync();
+            l1.ItemsSource = datalist;
+
+            foreach (var x in datalist)
+            {
+                value = x.status.ToString();
+            }
+
+            if (value == "on")
+            {
+                pinValue = GpioPinValue.Low;
+                pin.Write(pinValue);
+                LED.Fill = redBrush;
+            }
+            else
+            {
+                pinValue = GpioPinValue.High;
+                pin.Write(pinValue);
+                LED.Fill = grayBrush;
+            }
+        }
+    }
+}
---
+++ IoTMobileServiceDemo/IoTMobileServiceDemo/powerstatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTMobileServiceDemo
+{
+    public class powerstatus
+    {
+        public string id { get; set; }
+
+        public string status { get; set; }
+    }
+}
